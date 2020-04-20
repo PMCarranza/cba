e57bbca4e08ab3bee631f4f24961790a7f7039a6
Mariño Carranza <marinocarranza@Marinos-MacBook-Pro.local>
2020-04-20 12:27:21 -0700

    adds style, changes logo, adds wrapper divs, turns off timing js for now

--- javascript/timing.js
+++ javascript/timing.js
@@ -4,7 +4,7 @@ var frequency = moment().day(2+14).format('dddd MMM Do');
 
 console.log('frequency ' + frequency);
 
-var nextMeet = $('<h2>Next serving day is </h2>');
+var nextMeet = $('<h2>Join us on </h2>');
 nextMeet.append(frequency);
 
 $('#serving').append(nextMeet);
