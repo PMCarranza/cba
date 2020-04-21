c7cf8120a7e17fe981cca651d879d985cdcadb5e
Mariño Carranza <marinocarranza@Marinos-MacBook-Pro.local>
2020-04-20 19:05:42 -0700

    comments out unnecessary code

--- javascript/timing.js
+++ javascript/timing.js
@@ -4,21 +4,21 @@
 
 function getDates() {
     var firstDate = moment().subtract(6, 'days').startOf('day').toString();
-    // console.log('date', firstDate);
+// console.log('date', firstDate);
 
-    var start = moment('2020-04-14');
-    // console.log('start ' + start);
+var start = moment('2020-04-14');
+// console.log('start ' + start);
 
-    var end = moment(start).day(2 + 14).format('dddd MMM Do');
-    // console.log('end date is ' + end);
+// var end = moment(start).day(2 + 14).format('dddd MMM Do');
+// console.log('end date is ' + end);
 
-    var frequency = moment(start).day(2 + 14).format('dddd MMM Do');
-    // console.log('frequency ' + frequency);
+// var frequency = moment(start).day(2 + 14).format('dddd MMM Do');
+// console.log('frequency ' + frequency);
 
-    var nextMeet = $('<h2>Join us on </h2>');
-    // nextMeet.append(frequency);
+var nextMeet = $('<h2>Join us on </h2>');
+// nextMeet.append(frequency);
 
-    // $('#serving').append(nextMeet);
+// $('#serving').append(nextMeet);
 
     for (var i = 0; i < 1; i++) {
         var repeat = moment(start).day(2 + 14);
