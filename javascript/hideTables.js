9902e4d2c0ca5ef0ee9fcd9175a7683109d5a2e4
Mariño Carranza <marinocarranza@Marinos-MacBook-Pro.local>
2020-05-18 20:12:53 -0700

    tweaks code to clear tables based on day of the week not date

--- javascript/hideTables.js
+++ javascript/hideTables.js
@@ -1,25 +1,32 @@
 'use strict';
 
-console.log('hidetables');
 
-// variable gets the day of the week to use in the switch statement
-var day = (moment().weekday());
+// variable gets the day of the week to use in the if statement
+var day = (moment().weekday(2).format('DD'));
+
+// var day = (moment().day());
 console.log('day -> ' + day);
 
 function clearTable() {
-    if (day + 21 > 24 ) {
+    if (day > 23) {
+        console.log('>23 fourth ' + day);
         $('#fourth-body').hide();
-    } else if
-        (day + 14 > 17) {
-            console.log('third condition');
         $('#third-body').hide();
-        $('#second-body').hide();
-    } else if (day + 7 > 10) {
         $('#second-body').hide();
         $('#first-body').hide();
     } else if
-        (day > 3) {
-            console.log('first condition');
+        (day > 16) {
+        console.log('>16 second ' + day);
+        // $('#third-body').hide();
+        $('#second-body').hide();
+        $('#first-body').hide();
+    } else if (day + 9 > 9) {
+        console.log('>9 third ' + day);
+        $('#second-body').hide();
+        $('#first-body').hide();
+    } else if
+        (day + 2 > 2) {
+            console.log('>2 last ' + day);
             $('#first-body').hide();
     };
 
