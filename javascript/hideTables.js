afbd6225b05dc3197b2d8687f8372fb9ff6e43ae
Mariño Carranza <marinocarranza@Marinos-MacBook-Pro.local>
2020-05-20 19:47:53 -0700

    updates tables and timing code

--- javascript/hideTables.js
+++ javascript/hideTables.js
@@ -1,35 +1,31 @@
 'use strict';
 
-
 // variable gets the day of the week to use in the if statement
-var day = (moment().weekday(2).format('DD'));
-
-// var day = (moment().day());
-console.log('day -> ' + day);
+var day = (moment().weekday(3).format('d'));
+var weekday = parseInt(day);
 
 function clearTable() {
-    if (day > 23) {
-        console.log('>23 fourth ' + day);
-        $('#fourth-body').hide();
+    if (weekday + 21 > 24) {
+        console.log('>23 last ' + weeday);
+        $('#first-body').hide();
+        $('#second-body').hide();
         $('#third-body').hide();
-        $('#second-body').hide();
+        $('#fourth-body').hide();
+    } else if (weekday + 14 > 16) {
+        console.log('>16 third ' + weekday);
         $('#first-body').hide();
+        $('#second-body').hide();
+        $('#third-body').hide();
+    }
+    else if (weekday + 7 > 9) {
+        console.log('>9 third ' + weekday);
+        $('#first-body').hide();
+        $('#second-body').hide();
     } else if
-        (day > 16) {
-        console.log('>16 second ' + day);
-        // $('#third-body').hide();
-        $('#second-body').hide();
+        (weekday > 2) {
+        console.log('>2 last ' + weekday);
         $('#first-body').hide();
-    } else if (day + 9 > 9) {
-        console.log('>9 third ' + day);
-        $('#second-body').hide();
-        $('#first-body').hide();
-    } else if
-        (day + 2 > 2) {
-            console.log('>2 last ' + day);
-            $('#first-body').hide();
     };
-
 };
 
 clearTable();
