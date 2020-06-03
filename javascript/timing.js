b4324c6ec6744251075992f7e7c218ec4d611b58
Mariño Carranza <marinocarranza@Marinos-MacBook-Pro.local>
2020-06-02 18:08:34 -0700

    timing logic tweak to change tuesday date to next tuesday on this weeks tuesday

--- javascript/timing.js
+++ javascript/timing.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var servingDay = moment().day(2);
-    // console.log('servingDay ' + servingDay.format('MMM dd DD YYYY'));
+    console.log('servingDay ' + servingDay);
 
 // dom element to append the next serving date
 var nextMeet = $('<h2>Join us on </h2>');
@@ -9,8 +9,8 @@ var nextMeet = $('<h2>Join us on </h2>');
 // declaring variable to hold the next serving date
 var repeat;
 
-var today = moment();
-// console.log('today ' + today);
+var today = moment().day();
+console.log('today ' + today);
 
 // function gets the date of tuesday to serve
 function getDates() {
@@ -18,11 +18,12 @@ function getDates() {
     // creating dom element to append the next serving date
     $('#serving').append(nextMeet);
     // if it is tuesday add a week to serving date and append to dom
-    if (moment(today).isSame(servingDay)) {
+    if (moment(today).isSame(2)) {
         // add one week
         repeat = servingDay.add(1, 'week');
         // variable gets the formatted date
         var formatted = moment(repeat).format('dddd MMM Do');
+        console.log('formatted -> ' + formatted);
         // appends the formatted date to the header element
         nextMeet.append(formatted);
         // appends header to its containing div
